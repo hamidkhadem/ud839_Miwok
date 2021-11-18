b7738acc0968a4f0864b3f53fe4c36122aa0391f
hkh1370 <khadem.hamid@gmail.com>
2021-11-18 16:04:05 +0330

    Go to one layout

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,19 @@
+package com.example.android.miwok;
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,82 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+//    private static final String LOG_TAG = AndroidFlavorAdapter.class.getSimpleName();
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param words A List of AndroidFlavor objects to display in a list
+     */
+
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        defaultTextView.setText(currentWord.getmDefaultTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        miwokTextView.setText(currentWord.getmMiwokTranslation());
+/*
+
+        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        // Get the image resource ID from the current AndroidFlavor object and
+        // set the image to iconView
+        iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
+*/
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
